e49c60482498e2bee72befc7a2b6f9adf29562bc
Quintin Dong <quintindong@gmail.com>
2025-03-17 16:52:43 -0700

    walsls

---
+++ ccc25j1.cpp
@@ -0,0 +1,31 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <map>
+#include <vector> 
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <string>
+#include <iomanip>
+#include <queue>
+#include <set>
+using namespace std;
+#define ll long long
+
+
+int main() {
+
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);  // when submit remember to DELETE THESE AHHHHH
+
+    int place, a, b;
+    cin >> place >> a >> b;
+
+    if (place > a*b) {
+        cout << "no";
+    }
+    else {
+        cout << "yes";
+    }
+}
