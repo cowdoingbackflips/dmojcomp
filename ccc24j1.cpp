651c8cf2c50e590f1296ffb5ebc704672061ec60
Quintin Dong <quintindong@gmail.com>
2025-03-17 16:53:43 -0700

    ccc25j1 first

--- ccc24j1.cpp
+++ ccc24j1.cpp
@@ -15,5 +15,25 @@ using namespace std;
 
 
 int main() {
-    cout << "hellobello";
+
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);  // when submit remember to DELETE THESE AHHHHH
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+
+        int count = 0;
+
+        for (int i = 0; i < str.length()-1; i++) {
+            if (str[i] == str[i+1]) {
+                count++;
+            }
+        }
+
+        cout << str.length() - count << "\n";
+    }
 }
